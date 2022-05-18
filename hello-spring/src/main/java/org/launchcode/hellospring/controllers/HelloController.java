5b5a25f7e70741dae77fc75d69235bdfe6f2f774
Erin <mddscudder44@gmail.com>
2022-05-18 14:13:00 -0400

    done

--- hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,24 +1,59 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@ResponseBody
+@RequestMapping("hello")
 public class HelloController {
     //handles request at path /hello
-    @GetMapping("hello")
+    @GetMapping("hey")
     //Should handle get requests
-    @ResponseBody
     //Will return a plain text
 
     public String hello() {
         return "Hello, Spring!";
     }
 
+    //After moving the @RequestMapping this now lives at /hello/goodbye
     @GetMapping("goodbye")
-    @ResponseBody
     public String goodbye() {
         return "Goodbye, Spring";
     }
+
+    //handles request of the form /hello?name=LaunchCode
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
+    //name on 26 has to be the same as the requestparam on 30
+    public String helloWithQueryParam(@RequestParam String name) {
+        return "Hello, " + name + "!";
+    }
+
+    //handles requests of the form /hello/LaunchCode
+    //is part of the path now....not the query
+    @GetMapping("{name}")
+    public String helloWithPathParam(@PathVariable String name) {
+        return "Hello, " + name + "!";
+
+    }
+
+
+
+    //SENDING FORM DATA 10.3
+
+    //now lives at /hello/form
+    @GetMapping("form")
+
+    public String helloForm() {
+        return "<html>" +
+                "<body>" +
+                "<form action = 'hello' method = 'post'>" + //submit request to /hello
+                "<input type = 'text' name = 'name'>" +
+                "<input type = 'submit' value = 'Greet me!'>" +
+                "</form>" +
+                "</body>" +
+                "</html>";
+    }
+
 }
+
