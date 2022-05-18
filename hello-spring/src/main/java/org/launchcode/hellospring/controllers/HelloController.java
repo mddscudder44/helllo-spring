1c5f9d51ec7ed69c268bd0fb61c72bbae494d4bf
Erin <mddscudder44@gmail.com>
2022-05-18 13:19:36 -0400

    done

---
+++ hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -0,0 +1,24 @@
+package org.launchcode.hellospring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    //handles request at path /hello
+    @GetMapping("hello")
+    //Should handle get requests
+    @ResponseBody
+    //Will return a plain text
+
+    public String hello() {
+        return "Hello, Spring!";
+    }
+
+    @GetMapping("goodbye")
+    @ResponseBody
+    public String goodbye() {
+        return "Goodbye, Spring!";
+    }
+}
